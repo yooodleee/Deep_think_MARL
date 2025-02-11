92876fb919b9d9bff243e18a8621d478097eb741
yooodleee <accia25@naver.com>
2025-02-11 21:45:44 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/solvers/ForwardCheckingSolver.h
@@ -0,0 +1,23 @@
+
+#ifndef CSP_FORWARDCHECKINGSOLVER_H
+#define CSP_FORWARDCHECKINGSOLVER_H
+
+#include "Solver.h"
+
+typedef Environment::iterator EnvIt;
+
+
+class ForwardCheckingSolver : public Solver {
+public:
+    ForwardCheckingSolver(bool findAll);
+    virtual std::vector<Environment> solve(CspProblem7 problem) override;
+
+
+private:
+    std::vector<StackMachine>* constraintMachines;
+    bool forwardChecking(Environment&, const EnvIt&);
+    void retrieveDomainElements(Environment& state, EnvIt fcIt, list<list<pair<int, domain_element>>>& buffer);
+};
+
+
+#endif //CSP_FORWARDCHECKINGSOLVER_H
