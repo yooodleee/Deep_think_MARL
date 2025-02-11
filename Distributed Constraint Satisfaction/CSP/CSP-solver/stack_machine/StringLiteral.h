83e3bbb4c4b4f09ec3c7cd0b5ff3b17dbce5cfa6
yooodleee <accia25@naver.com>
2025-02-11 22:25:40 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/stack_machine/StringLiteral.h
@@ -0,0 +1,21 @@
+
+#ifndef CSP_STRINGLITERAL_H
+#define CSP_STRINGLITERAL_H
+
+
+#include "Expression.h"
+
+
+class StringLiteral : public Expression {
+public:
+    StringLiteral(std::string value);
+    virtual boost::optional<domain_element> eval(StackIt& it, Environment& environment);
+    std::string value;
+
+    virtual std::string describe() override {
+        return value;
+    };
+};
+
+
+#endif //CSP_STRINGLITERAL_H
