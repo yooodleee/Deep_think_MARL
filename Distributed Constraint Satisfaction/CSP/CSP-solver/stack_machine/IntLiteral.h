bca531f912e532ab4411c8824f5e65d248cd23a7
yooodleee <accia25@naver.com>
2025-02-11 22:13:24 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/stack_machine/IntLiteral.h
@@ -0,0 +1,21 @@
+
+
+#ifndef CSP_INTVALUE_H
+#define CSP_INTVALUE_H
+
+#include "Expression.h"
+
+
+class IntLiteral : public Expression {
+public:
+    IntLiteral(int value);
+    virtual boost::optional<domain_element> eval(StackIt& it, Environment& environment);
+    int value;
+
+    virtual std::string describe() override {
+        return std::to_string(value);
+    };
+};
+
+
+#endif //CSP_INTVALUE_H
