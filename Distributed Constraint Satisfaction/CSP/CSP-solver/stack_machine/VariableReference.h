b0e5caeaa716b0f29c18fb22ed4a65ba2a9e8603
yooodleee <accia25@naver.com>
2025-02-11 22:32:05 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/stack_machine/VariableReference.h
@@ -0,0 +1,21 @@
+
+#ifndef CSP_VARIABLEREFERENCE_H
+#define CSP_VARIABLEREFERENCE_H
+
+#include <string>
+#include "Expression.h"
+
+
+class VariableReference : public Expression {
+public:
+    VariableReference(std::string name);
+    virtual boost::optional<domain_element> eval(StackIt stack, Environment& environment) override;
+
+    virtual std::string describe() override {
+        return name;
+    };
+    std::string name;
+};
+
+
+#endif //CSP_VARIABLEREFERENCE_H
