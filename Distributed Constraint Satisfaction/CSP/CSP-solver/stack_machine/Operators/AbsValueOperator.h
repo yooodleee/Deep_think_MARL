8d239eac39ea168b0237633133c429fb171dd9d9
yooodleee <accia25@naver.com>
2025-02-11 22:38:01 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/stack_machine/Operators/AbsValueOperator.h
@@ -0,0 +1,18 @@
+
+#ifndef CSP_ABSVALUEOPERATOR_H
+#define CSP_ABSVALUEOPERATOR_H
+
+#include "../Expression.h"
+
+
+class AbsValueOperator : public Expression {
+    virtual boost::optional<domain_element> eval(StackIt stack, Environment& environment) override;
+
+    virtual std::string describe() override {
+        return "||";
+    };
+};
+
+
+
+#endif //CSP_ABSVALUEOPERATOR_H
