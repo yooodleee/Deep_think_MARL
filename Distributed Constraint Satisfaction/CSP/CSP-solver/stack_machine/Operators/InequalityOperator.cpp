59945ceb8f1fd314a808042ab4b888f2058f9bba
yooodleee <accia25@naver.com>
2025-02-12 09:32:59 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/stack_machine/Operators/InequalityOperator.cpp
@@ -0,0 +1,29 @@
+
+#include "InequalityOperator.h"
+#include "../IntLiteral.h"
+#include "../../utils/utils.h"
+
+#include <iostream>
+using namespace std;
+
+boost::optional<domain_element> InequalityOperator::eval(StackIt stack, Environment &environment) {
+    auto a = topAndPop(stack)->eval(stack, environment);
+    if (!a) {
+        return boost::none;
+    }
+    auto b = topAndPop(stack)->eval(stack, environment);
+    if (!b) {
+        return boost::none;
+    }
+
+    auto lhs = a.get();
+    auto rhs = b.get();
+
+    if (lhs.type() == typeid(int)) {
+        return boost::optional<domain_element>(boost::get<int>(lhs) != boost::get<int>(rhs));
+    }
+
+    if (lhs.type() == typeid(bool)) {
+        return boost::optional<domain_element>(boost::get<bool>(lhs) != boost::get<bool>(rhs));
+    }
+}
