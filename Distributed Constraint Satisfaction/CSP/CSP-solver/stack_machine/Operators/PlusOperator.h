b6a286f5903fd06b579d8bc2c7762d749e34f8d4
yooodleee <accia25@naver.com>
2025-02-12 09:45:17 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/stack_machine/Operators/PlusOperator.h
@@ -0,0 +1,18 @@
+
+#ifndef CSP_PLUSOPERATOR_H
+#define CSP_PLUSOPERATOR_H
+
+#include "../Expression.h"
+
+
+class PlusOperator : public Expression {
+    virtual boost::optional<domain_element> eval(StackIt stack, Environment& environment) override;
+
+    virtual std::string describe() override {
+        return "+";
+    };
+};
+
+
+
+#endif //CSP_PLUSOPERATOR_H
