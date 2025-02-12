4b839a32c2963f38e3ef23af5a90473131d56faa
yooodleee <accia25@naver.com>
2025-02-12 09:48:27 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/stack_machine/Operators/SubscriptOperator.cpp
@@ -0,0 +1,18 @@
+
+#include "SubscriptOperator.h"
+#include "../../utils/utils.h"
+
+boost::optional<domain_element> SubscriptOperator::eval(StackIt stack, Environment &environment) {
+    auto index_o = topAndPop(stack)->eval(stack, environment);
+    if (!index_o)
+        return boost::none;
+
+    auto str_o = topAndPop(stack)->eval(stack, environment);
+    if (!str_o)
+        return boost::none;
+
+    auto index = boost::get<int>(index_o.get());
+    auto str = boost::get<std::string>(str_o.get());
+
+    return boost::optional<domain_element>(str.substr(index, 1));
+}
