2d26fdeea279ee5cc2b94fdefc7acf0e8f8ae732
yooodleee <accia25@naver.com>
2025-02-12 09:43:04 +0900

    code

---
+++ Distributed Constraint Satisfaction/CSP/CSP-solver/stack_machine/Operators/PlusOperator.cpp
@@ -0,0 +1,14 @@
+
+
+#include "PlusOperator.h"
+#include "../../utils/utils.h"
+
+boost::optional<domain_element> PlusOperator::eval(StackIt stack, Environment &environment) {
+    auto a = topAndPop(stack)->eval(stack, environment);
+    if (!a) return boost::none;
+
+    auto b = topAndPop(stack)->eval(stack, environment);
+    if (!b) return boost::none;
+
+    return boost::optional<domain_element>(boost::get<int>(a.get()) + boost::get<int>(b.get()));
+}
