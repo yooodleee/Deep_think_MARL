bc83a44cc2035ca6415a07d3d19252d8a7cfc5ed
yooodleee <accia25@naver.com>
2025-02-15 13:42:37 +0900

    code

--- Distributed Constraint Satisfaction/Domain-pruning algo/core/Constraints/Generic/ConstraintExtN.hh
+++ Distributed Constraint Satisfaction/Domain-pruning algo/core/Constraints/Generic/ConstraintExtN.hh
@@ -13,3 +13,35 @@ struct ExtSave {
 };
 
 
+class ConstraintExtN : public Constraint {
+private:
+    std::vector<int> sSup;
+    std::vector<int> sVal;
+
+    std::vector<std::vector<indVp>> tuples;
+
+    size_t currentLimit;
+
+    std::stack<ExtSave> saveSize;
+    std::vector<size_t> position;
+    std::vector<int> lastSize;
+
+    void restoreTuples(int level);
+    void removeTuple(size_t tuplePos, int level);
+    void isValidTuple(std::vector<ind> tuple);
+
+
+public:
+    ConstraintExtN(std::string n, std::vector<Variable*> vars, std::vector<std::vector<ind>>& t);
+
+    void backtrack(int level) {
+        restoreTuples(level);
+    }
+
+    bool init();
+    bool propagate(int level, Variable* cur, std::vector<Variable*>& touched);
+};
+
+
+
+#endif  // CONSTRAINTEXTN_H_
