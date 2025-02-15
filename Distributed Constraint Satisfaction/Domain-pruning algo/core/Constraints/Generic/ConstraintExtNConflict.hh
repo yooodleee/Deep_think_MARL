57fa3ccd2fac950b347e0f753ce2930befe3162b
yooodleee <accia25@naver.com>
2025-02-15 13:44:23 +0900

    code

---
+++ Distributed Constraint Satisfaction/Domain-pruning algo/core/Constraints/Generic/ConstraintExtNConflict.hh
@@ -0,0 +1,14 @@
+
+#ifndef CONSTRAINTEXTNCONFLICT_H_
+#define CONSTRAINTEXTNCONFLICT_H_
+
+#include "Constraint.hh"
+#include "Variable.hh"
+
+
+struct ExtConflictSave {
+    int level;
+    size_t sizeDom;
+};
+
+
