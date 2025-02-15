0bbba9b0a259c2b16bfee1ccd7bdbc7599822c0f
yooodleee <accia25@naver.com>
2025-02-15 14:46:18 +0900

    code

--- Distributed Constraint Satisfaction/Domain-pruning algo/core/Constraints/Summing/ConstraintSum.hh
+++ Distributed Constraint Satisfaction/Domain-pruning algo/core/Constraints/Summing/ConstraintSum.hh
@@ -64,3 +64,46 @@ public:
 };
 
 
+class ConstraintSumNE : public ConstraintSum {
+private:
+    int sentinel1;
+    int sentinel2;
+
+    inline long currentSum()
+    {
+        long sum = 0;
+        for (size_t i = 0; i < listSize; i++)
+            sum += scope[i]->LocalDomIndToVal(0);
+        return sum;
+    }
+
+    inline long currentSumExcept(int except)
+    {
+        long sum = 0;
+        for (int i = 0; i < (int)listSize; i++)
+            if (i != except)
+                sum += scope[i]->LocalDomIndToVal(0);
+        return sum;
+    }
+
+    inline int findAnotherSentinel()
+    {
+        for (int i = 0; i < (int)listSize; i++)
+            if (i != sentinel1 && != sentinel2 && !scope[i]->isAssigned())
+                return i;
+        return -1;
+    }
+
+
+public:
+    ConstraintSumNE(std::string n, std::vector<Variable*> vars, int lim)
+        : ConstraintSum(n, vars, lim)
+        , sentinel1(0)
+        , sentinel2(vars.size() - 1){};
+
+    bool propagate(int level, Variable* cur, std::vector<Variable*>& touched);
+};
+
+
+
+#endif  // CONSTRAINTSUM_H_
