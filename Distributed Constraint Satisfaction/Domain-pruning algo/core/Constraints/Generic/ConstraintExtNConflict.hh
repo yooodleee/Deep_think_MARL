25fa4915776d91e98e5d49cdde285960f000689b
yooodleee <accia25@naver.com>
2025-02-15 13:48:04 +0900

    code

--- Distributed Constraint Satisfaction/Domain-pruning algo/core/Constraints/Generic/ConstraintExtNConflict.hh
+++ Distributed Constraint Satisfaction/Domain-pruning algo/core/Constraints/Generic/ConstraintExtNConflict.hh
@@ -12,3 +12,34 @@ struct ExtConflictSave {
 };
 
 
+class ConstraintExtNConflict : public Constraint {
+private:
+    std::vector<std::vector<indVp>> tuples;
+
+    size_t currentLimit;
+
+    std::stack<ExtConflictSave> saveSize;
+    std::vector<size_t> position;
+    std::vector<std::vector<size_t>> timesSeen;
+
+    void restoreTuples(int level);
+    void removeTuple(size_t tuplePos, int level);
+    bool isValidTuple(std::vector<ind> tuple);
+
+
+public:
+    ConstraintExtNConflict(std::string n, std::vector<Variable*> vars, std::vector<std::vector<ind>>& t);
+
+    void backtrack(int level) {
+        restoreTuples(level);
+    }
+
+    bool init();
+    bool propagate(int level, Variable* cur, std::vector<Variable*>& touched);
+
+    void debug();
+};
+
+
+
+#endif  // CONSTRAINTEXTNCONFLICT_H_
