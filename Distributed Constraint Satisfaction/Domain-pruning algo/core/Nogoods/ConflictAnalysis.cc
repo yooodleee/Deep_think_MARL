899ffd10f650c16eae2e5b062ef9f543518e5e71
yooodleee <accia25@naver.com>
2025-02-17 19:35:25 +0900

    buildStack

--- Distributed Constraint Satisfaction/Domain-pruning algo/core/Nogoods/ConflictAnalysis.cc
+++ Distributed Constraint Satisfaction/Domain-pruning algo/core/Nogoods/ConflictAnalysis.cc
@@ -27,3 +27,28 @@ void ConflictAnalysis::printHeaderAnalyse(Constraint* ctr)
 }
 
 
+void ConflictAnalysis::buildStack(vector<unsigned>& conflictCl, int level)
+{
+    for (auto lit : conflictCl) {
+        indVp ivp = lit >> 1;
+        Expl& cur = Variable::vpExpl[ivp];
+
+        assert(Variable::varProps[ivp].state != UNDEF);
+
+        if (!cur.level) // level 0 decision, does not belong to explanation
+            continue;   // conflictCl isn't ordered by level, so continue instead of breaking
+        else if (cur.level == level) {
+            if (Options::Verbose >= verbose::high)
+                cerr << "is+" << ((lit & 1) ? "¬" : "") << ivp << " ";
+            stack.insert(make_pair(lit, &cur)); // put the same sign as the conflictCl because it's handled by getConflict method
+        } else {
+            assert(!Variable::varProps[ivp].marked);
+            Variable::varProps[ivp].marked = true;
+            if (Options::Verbose >= verbose::high)
+                cerr << "ic+" << ((lit & 1) ? "¬" : "") << ivp << " ";
+            cl.push_back(lit);  // put the same sign as the conflictCl because it's handled by getConflict method
+        }
+    }
+}
+
+
