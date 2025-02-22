08672c9970fa2f563cf3fe8fc6a8d16dc9500fe9
yooodleee <accia25@naver.com>
2025-02-22 17:22:39 +0900

    Luby

--- Distributed Constraint Satisfaction/Domain-pruning algo/core/src/Restart.cc
+++ Distributed Constraint Satisfaction/Domain-pruning algo/core/src/Restart.cc
@@ -44,3 +44,18 @@ void Restart::Inc()
 }
 
 
+
+int Restart::Luby(int i)
+{
+    int k = 1;
+    int power = 2;
+
+    /// let 2^k be the least power of 2 >= (i+1)
+    for (; power < (i + 1); k += 1, power *= 2)
+        ;
+    
+    if (power == i + 1)
+        return power / 2;
+    
+    return Luby(i - (power / 2) + 1);
+}
