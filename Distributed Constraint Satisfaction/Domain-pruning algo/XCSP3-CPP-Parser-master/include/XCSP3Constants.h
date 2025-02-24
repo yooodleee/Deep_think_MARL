a60b16fae955fb44cf468bdde4f5e0d0ca459269
yooodleee <accia25@naver.com>
2025-02-24 17:00:48 +0900

    code

---
+++ Distributed Constraint Satisfaction/Domain-pruning algo/XCSP3-CPP-Parser-master/include/XCSP3Constants.h
@@ -0,0 +1,100 @@
+
+#ifndef XCSP3CONSTANTS_H_
+#define XCSP3CONSTANTS_H_
+
+
+#include <climits>
+
+
+namespace XCSP3Core {
+    typedef enum {
+        CSP, 
+        COP
+    } InstanceType;
+
+
+    typedef enum CONSTRAINTS {
+        UNKNOWN, 
+        EXTENSION, 
+        INTENSION, 
+        ALLDIFF, 
+        ALLEQUAL, 
+        SUM, 
+        ORDERED, 
+        COUNT, 
+        NVALUES, 
+        CARDINALITY, 
+        MAXIMUM, 
+        MINIMUM,
+        ELEMENT, 
+        NOOVERLAP, 
+        STRETCH, 
+        LEX, 
+        CHANNEL, 
+        REGULAR, 
+        MDD, 
+        CUMULATIVE, 
+        INSTANTIATION, 
+        CIRCUIT
+    } ConstraintType;
+
+
+    typedef enum order {
+        LE,
+        LT,
+        GE,
+        GT,
+        IN,
+        EQ,
+        NE
+    } OrderType;
+
+
+    typedef enum operandtype {
+        INTEGER,
+        INTERVAL,
+        VARIABLE
+    } OperandType;
+
+
+    typedef enum tag {
+        UNKNOWNTAG, 
+        LISTTAG, 
+        FUNCTIONALTAG, 
+        VALUESTAG, 
+        VALUETAG, 
+        CONDITIONTAG, 
+        INDEXTAG, 
+        LENGTHSTAG
+    } Tag;
+
+
+    typedef enum ranktype {
+        ANY,
+        FIRST,
+        LAST
+    } RankType;
+
+
+    typedef enum objective {
+        MINIMIZE,
+        MAXIMIZE
+    } ObjectiveGoal;
+
+
+    typedef enum expressionObjective {
+        EXPRESSION_O_,
+        SUM_O_,
+        PRODUCT_O_,
+        MINIMUM_O_,
+        MAXIMUM_O_,
+        NVALUES_O_,
+        LEX_O
+    } ExpressionObjective;
+
+#define STAR INT_MAX
+}
+
+
+
+#endif  // XCSP3CONSTANTS_H_
