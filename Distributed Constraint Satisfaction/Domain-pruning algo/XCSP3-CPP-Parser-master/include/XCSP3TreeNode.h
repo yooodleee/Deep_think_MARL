f0906fa96944ae47e7fe9733960f0e629b3ea7be
yooodleee <accia25@naver.com>
2025-02-28 19:24:40 +0900

    code

--- Distributed Constraint Satisfaction/Domain-pruning algo/XCSP3-CPP-Parser-master/include/XCSP3TreeNode.h
+++ Distributed Constraint Satisfaction/Domain-pruning algo/XCSP3-CPP-Parser-master/include/XCSP3TreeNode.h
@@ -287,5 +287,326 @@ namespace XCSP3Core {
     };
 
 
-    
+    // ##############################################################
+    // Node for Sub, Div, Mod, Pow, Dist
+    // ##############################################################
+
+
+    class NodeSub : public NodeBinary {
+    public:
+        NodeSub() : NodeBinary("sub", OSUB) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) - parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeDiv : public NodeBinary {
+    public:
+        NodeDiv() : NodeBinary("div", ODIV) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) / parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeMod : public NodeBinary {
+    public:
+        NodeMod() : NodeBinary("mod", OMOD) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) % parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodePow : public NodeBinary {
+    public:
+        NodePow() : NodeBinary("pow", OPOW) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            return pow(parameters[0]->evaluate(tuple), parameters[1]->evaluate(tuple));
+        }
+    };
+
+
+    class NodeDist : public NodeBinary {
+    public:
+        NodeDist() : NodeBinary("dist", ODIST) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int v = parameters[0]->evaluate(tuple) - parameters[1]->evaluate(tuple);
+            return v > 0 ? v : -v;
+        }
+    };
+
+
+    // ##############################################################
+    // Node for LE, LT, GE, GT, NE, Imp
+    // ##############################################################
+
+
+    class NodeLE : public NodeBinary {
+    public:
+        NodeLE() : NodeBinary("le", OLE) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) <= parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeLT : public NodeBinary {
+    public:
+        NodeLT() : NodeBinary("lt", OLT) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) < parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeGE : public NodeBinary {
+    public:
+        NodeGE() : NodeBinary("ge", OGE) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) >= parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeGT : public NodeBinary {
+    public:
+        NodeGT() : NodeBinary("gt", OGT) {}
+
+        int evaluate(std::map<string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) > parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeNE : public NodeBinary {
+    public:
+        NodeNE() : NodeBinary("ne", ONE) {}
+
+        int evaluate(std::map<string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) != parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeImp : public NodeBinary {
+    public:
+        NodeImp() : NodeBinary("impl", OIMP) {}
+
+        int evaluate(std::map<string, int>& tuple) override {
+            return parameters[0]->evaluate(tuple) == 0 || parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    // ##############################################################
+    // Node for Add, Mult, Min, Max
+    // ##############################################################
+
+
+    class NodeAdd : public NodeNAry {
+    public:
+        NodeAdd() : NodeNAry("add", OADD) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int nb = 0;
+            for (unsigned int i = 0; i < parameters.size(); i++)
+                nb += parameters[i]->evaluate(tuple);
+            return nb;
+        }
+    };
+
+
+    class NodeMult : public NodeNAry {
+    public:
+        NodeMult() : NodeNAry("mul", OMUL) {}
+
+        int evaluate(std::map<string, int>& tuple) override {
+            int nb = 1;
+            for (unsigned int i = 0; i < parameters.size(); i++)
+                nb *= parameters[i]->evaluate(tuple);
+            return nb;
+        }
+    };
+
+
+    class NodeMin : public NodeNAry {
+    public:
+        NodeMin() : NodeNAry("min", OMIN) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int nb = parameters[0]->evaluate(tuple);
+            for (unsigned int i = 1; i < parameters.size(); i++) {
+                int v = parameters[i]->evaluate(tuple);
+                if (v < nb) nb = v;
+            }
+            return nb;
+        }
+    };
+
+
+    class NodeMax : public NodeNAry {
+    public:
+        NodeMax() : NodeNAry("max", OMAX) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int nb = parameters[0]->evaluate(tuple);
+            for (unsigned int i = 1; i < parameters.size(); i++) {
+                int v = parameters[i]->evaluate(tuple);
+                if (v > nb) nb = v;
+            }
+            return nb;
+        }
+    };
+
+
+    // ##############################################################
+    // Node for EQ, And, Or, Xor, Iff
+    // ##############################################################
+
+
+    class NodeEQ : public NodeNAry {
+    public:
+        NodeEQ() : NodeNAry("eq", OEQ) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int nb = parameters[0]->evaluate(tuple);
+            for (unsigned int i = 1; i < parameters.size(); i++) {
+                if (nb != parameters[i]->evaluate(tuple))
+                    return 0;
+            }
+            return 1;
+        }
+    };
+
+
+    class NodeAnd : public NodeNAry {
+    public:
+        NodeAnd() : NodeNAry("and", OAND) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            for (unsigned int i = 0; i < parameters.size(); i++) 
+                if (!parameters[i]->evaluate(tuple))
+                    return 0;
+            return 1;
+        }
+    };
+
+
+    class NodeOr : public NodeNAry {
+    public:
+        NodeOr() : NodeNAry("or", OOR) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            for (unsigned int i = 0; i < parameters.size(); i++)
+                if (parameters[i]->evaluate(tuple)) {
+                    return 1;
+                }
+            return 0;
+        }
+    };
+
+
+    class NodeXor : public NodeNAry {
+    public:
+        NodeXor() : NodeNAry("xor", OXOR) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int nb = 0;
+            for (unsigned int i = 0; i < parameters.size(); i++)
+                nb = nb + parameters[i]->evaluate(tuple);
+            return nb % 2 == 1;
+        }
+    };
+
+
+    class NodeIf : public NodeNAry {
+    public:
+        NodeIf() : NodeNAry("if", OIF) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int nb = parameters[0]->evaluate(tuple);
+            if (nb) return parameters[1]->evaluate(tuple);
+            return parameters[2]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeIff : public NodeNAry {
+    public:
+        NodeIff() : NodeNAry("iff", OIFF) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            assert(parameters.size() == 2); // TODO if greater!!
+            int nb = parameters[0]->evaluate(tuple);
+            return (nb) ? parameters[1]->evaluate(tuple) : !parameters[1]->evaluate(tuple);
+        }
+    };
+
+
+    class NodeSet : public NodeNAry {
+    public:
+        NodeSet() : NodeNAry("set", OSET) {}
+
+        // std::map<std::string, int>& tuple
+        int evaluate(std::map<std::string, int>& ) override {
+            throw std::runtime_error("can't evaluate set");
+        }
+    };
+
+
+    class NodeIn : public NodeBinary {
+    protected:
+        std::vector<int> set;
+
+    public:
+        NodeIn() : NodeBinary("in", OIN) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int nb = parameters[0]->evaluate(tuple);
+            set.clear();
+            NodeSet* nodeSet;
+
+            if ((nodeSet = dynamic_cast<NodeSet*>(parameters[1])) == NULL)
+                throw std::runtime_error("intension constraint : in requires a set as second parameter");
+            for (unsigned int i = 0; i < nodeSet->parameters.size(); i++)
+                set.push_back(nodeSet->parameters[i]->evaluate(tuple));
+            return find(set.begin(), set.end(), nb) != set.end();
+        }
+    };
+
+
+    class NodeNotIn : public NodeBinary {
+    protected:
+        std::vector<int> set;
+
+    public:
+        NodeNotIn() : NodeBinary("notin", ONOTIN) {}
+
+        int evaluate(std::map<std::string, int>& tuple) override {
+            int nb = parameters[0]->evaluate(tuple);
+            set.clear();
+            NodeSet* nodeSet;
+
+            if ((nodeSet = dynamic_cast<NodeSet*>(parameters[1])) == NULL)
+                throw std::runtime_error("intension constraint : in requires a set as second parameter");
+            for (unsigned int i = 0; i < nodeSet->parameters.size(); i++)
+                set.push_back(nodeSet->parameters[i]->evaluate(tuple));
+            return find(set.begin(), set.end(), nb) == set.end();
+        }
+    };
+
 }
+
+
+
+#endif  // XCSP3PARSER_XCSP3TREENODE_H_
