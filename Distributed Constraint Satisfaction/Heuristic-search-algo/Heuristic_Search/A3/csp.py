5cfd0cf767f2b2f8ece0852273e95b67b10001c5
yooodleee <accia25@naver.com>
2025-03-14 19:54:42 +0900

    copy

--- Distributed Constraint Satisfaction/Heuristic-search-algo/Heuristic_Search/A3/csp.py
+++ Distributed Constraint Satisfaction/Heuristic-search-algo/Heuristic_Search/A3/csp.py
@@ -129,4 +129,161 @@ class Variable:
 
 # implement various types of constraints
 class Constraint:
+    """Base class for defining constraints. Each constraint can check if
+    it has been satisfied, so each type of constraint must be a different
+    class. For example a constraint of notEquals(V1, V2) must be a dufferent
+    class from a constraint of greaterThan(V1, V2), as they must implement
+    checks of satisfaction.
     
+    However one can define a class of general table constraints, as below,
+    then can capture many different constraints.
+    
+    On initialization the constraint's name can be given as well as the 
+    constraint's scope. IMPORTANT, the scope is ordered! E.g., the constraint
+    greaterThan(V1, V2) is not the same as the constraint greaterThan(V2, V1).
+    """
+
+    def __init__(self, name, scope):
+        """Create a constraint obj, specify the constraint name (a string
+        ) and its scope (an ORDERED list of variable obj)."""
+        self._scope = list(scope)
+        self._name = "baseClass_" + name    # override in subconstraint types!
+    
+    def scope(self):
+        return list(self._scope)
+    
+    def arity(self):
+        return len(self._scope)
+    
+    def numUnassigned(self):
+        i = 0
+        for var in self._scope:
+            if not var.isAssigned():
+                i += 1
+        return i
+    
+    def unAssignedVars(self):
+        return [var for var in self.scope() if not var.isAssigned()]
+    
+    def check(self):
+        util.raiseNotDefined()
+    
+    def name(self):
+        return self._name
+    
+    def __str__(self):
+        return "Cnstr_{}({})".format(self.name(), list(map(lambda var: var.name(), self.scope())))
+    
+    def printConstraint(self):
+        print(
+            "Cons: {} Vars = {}".format(self.name(), [v.name() for v in self.scope()])
+        )
+
+
+# obj for holding a constraint prob
+class CSP:
+    """CSP class groups together a set of variables and a set of constraints to
+    form a CSP prob. Provides a useful place to put some other functions that
+    depend on which variables and constraints are active"""
+
+    def __init__(
+            self, name, variables, constraints
+    ):
+        """Create a CSP prob obj passing it a name a list
+        variables obj and a list of constraint obj"""
+
+        self._name = name
+        self._variables = variables
+        self._constraints = constraints
+
+        # some sanity checks
+        varsInCnst = set()
+        for c in constraints:
+            varsInCnst = varsInCnst.union(c.scope())
+
+        for v in variables:
+            if v not in varsInCnst:
+                print(
+                    "Warning: variable {} is not in any constraint of the CSP {}"
+                    .format(v.name(), self.name())
+                )
+        
+        for v in varsInCnst:
+            if v not in variables:
+                print(
+                    "Error: variable {} appears in constraint but specified as one of the variables of the CSP {}"
+                    .format(v.name(), self.name())
+                )
+        
+        self.constraints_of = [[] for i in range (len(variables))]
+        for c in constraints:
+            for v in c.scope():
+                i = variables.index(v)
+                self.constraints_of[i].append(c)
+    
+
+    def name(self):
+        return self._name
+    
+    def variables(self):
+        return list(self._variables)
+    
+    def constraints(self):
+        return list(self._constraints)
+    
+    def constraintsOf(self, var):
+        """return constraints with var in their scope"""
+        try:
+            i = self.variables().index(var)
+            return list(self.constraints_of[i])
+        except:
+            print(
+                "Error: tried to find constraint of variable {} that isn't in this CSP {}"
+                .format(var, self.name())
+            )
+    
+    def unAssignAllVars(self):
+        """unassign all variables"""
+        for v in self.variables():
+            v.unAssign()
+    
+    def check(self, solutions):
+        """each solution is a list of (var, value) pairs. Check to see
+        if these satisfy all the constraints. Return list of erroneous
+        solutions."""
+
+        # save values to restore later
+        current_values = [(var, var.getValue()) for var in self.variables()]
+        errs = []
+
+        for s in solutions:
+            s_vars = [var for (var, val) in s]
+
+            if len(s_vars) != len(self.variables()):
+                errs.append([s, "Solution has incorrect number of variables in it"])
+                continue
+
+            if len(set(s_vars)) != len(self.variables()):
+                errs.append([s, "Solution has duplicate Variable Assignments"])
+                continue
+
+            if set(s_vars) != set(self.variables()):
+                errs.append([s, "Solution has incorrect Variable in it"])
+                continue
+
+            for (var, val) in s:
+                var.setValue(val)
+            
+            for c in self.constraints():
+                if not c.check():
+                    errs.append([s, "Solution does not satisfy constraint {}".format(c.name())])
+                    break
+        
+        for (var, val) in current_values:
+            var.setValue(val)
+        
+        return errs
+    
+
+    def __str__(self):
+        return "CSP {}".format(self.name())
